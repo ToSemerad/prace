7b01baf7fb9a3f93643c9ee7a7db924bfcc978d9
ToSemerad <44479180+ToSemerad@users.noreply.github.com>
2023-06-27 21:37:30 +0200

    STS program pro tiskove rady
    java program pro tiskové řady spouští se pomocí WF pro překlad, který volá .bat

---
+++ STS/vyvoj/STS_Predklad_pdf/src/model/PrintAssembly.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+
+/**
+ *
+ * @author TPV
+ */
+public class PrintAssembly {
+    
+    private final List<Part> parts;
+    private final String fileName;
+    
+    public PrintAssembly(String fileName) {
+        parts = new ArrayList<>();
+        this.fileName = fileName;
+    }
+    
+    public void addPart(Part part) {
+        this.parts.add(part);
+    }
+    
+    public void save() throws IOException {
+        PDDocument docNew = new PDDocument();
+        
+        for (Part p : parts) {
+            PDDocument doc = p.getDocument();
+            PDPage p1 = doc.getPage(0);
+            PDPage p2 = doc.getPage(1);
+            
+            COSDictionary d1 = p1.getCOSObject();
+            COSDictionary d1c = new COSDictionary(d1);
+            d1c.removeItem(COSName.ANNOTS);
+            PDPage p1c = new PDPage(d1c);
+            docNew.addPage(p1c);
+            
+            
+            COSDictionary d2 = p2.getCOSObject();
+            COSDictionary d2c = new COSDictionary(d2);
+            d2c.removeItem(COSName.ANNOTS);
+            PDPage p2c = new PDPage(d2c);
+            docNew.addPage(p2c);
+            
+        }
+        
+        docNew.save(fileName);
+    }
+    
+    @Override
+    public String toString() {
+        String str = "*** SESTAVA TISKU ***\n";
+        
+        for (Part part : parts) {
+            str += "--------------------------\n";
+            str += "Díl číslo: " + part.getPartNumber() + "\n";
+            str += "Výkres: " + part.getDrawing() + "\n";
+            str += part.toString();
+            str += "--------------------------\n";
+        }
+        
+        str += "*** KONEC SESTAVY ***\n";
+        
+        return str;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public List<Part> getParts() {
+        return parts;
+    }
+    
+}
